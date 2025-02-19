233dffeb79b5341ea3ca79f3e79b86dbae6d9e26
aniket-8719 <aniket07013@gmail.com>
2025-02-20 03:21:11 +0530

    url

--- frontend/src/Components/ReferEarn/ParentComponent.js
+++ frontend/src/Components/ReferEarn/ParentComponent.js
@@ -6,7 +6,7 @@ const ParentComponent = () => {
 
     return (
         <div>
-            <button onClick={() => setIsModalOpen(true)} className="flex justify-center items-center mx-auto px-8 py-2 bg-blue-500 text-white rounded-lg mt-28">Refer a Friend 
+            <button onClick={() => setIsModalOpen(true)} className="flex justify-center items-center mx-auto px-12 py-4 bg-blue-500 text-white rounded-lg mt-28">Refer a Friend 
             </button>
                 <div className="flex justify-center items-center mx-auto">click & send</div>
             {isModalOpen && <ReferralModal onClose={() => setIsModalOpen(false)} />}
